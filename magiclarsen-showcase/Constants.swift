f40c5bafc60353cf291dcff5dde0a7aa4688554f
magicman18 <magiclarsen@gmail.com>
2016-05-03 09:29:39 -0700

    Created Basis for Social Network type app
    Learning firebase

---
+++ magiclarsen-showcase/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  magiclarsen-showcase
+//
+//  Created by Larsen Eisenberg on 5/3/16.
+//  Copyright © 2016 Magiclarsen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
