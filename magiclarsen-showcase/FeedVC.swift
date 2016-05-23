729aec5eb1d46ff463bee307f201591620634445
magicman18 <magiclarsen@gmail.com>
2016-05-23 09:06:14 -0700

    Added the FeedVC and got everything looking good on that

---
+++ magiclarsen-showcase/FeedVC.swift
@@ -0,0 +1,38 @@
+//
+//  FeedVC.swift
+//  magiclarsen-showcase
+//
+//  Created by Larsen Eisenberg on 5/23/16.
+//  Copyright © 2016 Magiclarsen. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+        
+    }
+
+
+}
---
+++ magiclarsen-showcase/PostCell.swift
@@ -0,0 +1,34 @@
+//
+//  PostCell.swift
+//  magiclarsen-showcase
+//
+//  Created by Larsen Eisenberg on 5/23/16.
+//  Copyright © 2016 Magiclarsen. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var showcaseImg: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    override func drawRect(rect: CGRect) {
+        profileImg.layer.cornerRadius = profileImg.frame.size.width / 2
+        profileImg.clipsToBounds = true
+        
+        showcaseImg.clipsToBounds = true
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
