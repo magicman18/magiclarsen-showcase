201f36746470ea6f4253bd8a107e1469c50c5fa6
magicman18 <magiclarsen@gmail.com>
2016-05-23 10:29:08 -0700

    added Firebase URL references and added code to add users to Firebase when they login

--- magiclarsen-showcase/ViewController.swift
+++ magiclarsen-showcase/ViewController.swift
@@ -45,6 +45,10 @@ class ViewController: UIViewController {
                             print("Login failed\(error)")
                         } else {
                             print("Logged In \(authData)")
+                            
+                            let user = ["provider": authData.provider!, "blah":"test"]
+                            DataService.ds.createFirebaseUser(authData.uid, user: user)
+                            
                             NSUserDefaults.standardUserDefaults().setValue(authData.uid, forKey: KEY_UID)
                             self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
                         }
@@ -72,7 +76,13 @@ class ViewController: UIViewController {
                             } else {
                                 NSUserDefaults.standardUserDefaults().setValue(result [KEY_UID], forKey: KEY_UID)
                                 
-                                DataService.ds.REF_BASE.authUser(email, password: pwd, withCompletionBlock: nil)
+                                DataService.ds.REF_BASE.authUser(email, password: pwd, withCompletionBlock: { err, authData in
+                                    let user = ["provider": authData.provider!, "blah":"test"]
+                                    DataService.ds.createFirebaseUser(authData.uid, user: user)
+                                })
+                                
+                                
+                            
                                 self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
                             
                             }
